afa517361907f0b1268bf16f31104c3f0862bbe3
Karin Lampesberger <karin@lampesberger.at>
2014-03-31 17:06:11 +0200

    tabelle

---
+++ VS/src/Threads/SearchNr.java
@@ -0,0 +1,17 @@
+package Threads;
+
+import java.util.ArrayList;
+
+import Tabellen.Person;
+
+public class SearchNr {
+	
+	//private int nr; 
+	private ArrayList<Person> foundNumbers;
+	
+	public SearchNr(int nr) {
+		//this.nr = nr; 
+	}
+
+	p
+}
