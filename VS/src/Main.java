835f7978ea3083e8b095e23163247eafb60fb738
Victor Pettelkau <victor_pettelkau@web.de>
2014-04-07 16:26:42 +0200

    gridlayout

--- VS/src/Main.java
+++ VS/src/Main.java
@@ -4,6 +4,7 @@ import UI.View;
 public class Main {
 	
 	public static void main (String[] args) {
+		@SuppressWarnings("unused")
 		View view = new View();
 	}
 
--- VS/src/Threads/SearchName.java
+++ VS/src/Threads/SearchName.java
@@ -1,5 +1,27 @@
 package Threads;
 
-public class SearchName {
+import java.util.ArrayList;
 
+import Tabellen.Person;
+import Tabellen.Tabelle;
+
+public class SearchName {
+	
+	private ArrayList<Person> foundNames;
+	
+	public SearchName(){
+		
+	}
+
+	public ArrayList<Person >search (String name){
+		
+		ArrayList<Person> table = new Tabelle().getList();
+		for (int i = 0; i < table.size(); i++ ){
+			if(name.equals(table.get(i).getName() )){
+				foundNames.add(table.get(i));
+			}
+			
+		}
+		return foundNames;
+	}
 }
